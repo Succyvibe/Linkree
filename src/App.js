f5ec65566e6e2310d9a31f6fde439a5281cfc373
success ibekwe <Successibekwe047@gmail.com>
2022-10-30 17:45:49 +0100

    added ProfilePic component for  users profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import LinkButton from "./components/LinkButton";
+import ProfilePic from "./components/ProfilePic";
 
 function App() {
   return (
     <div className="App w-screen h-screen bg-[#f4f5f7]">
-      <main className="w-[1440px] bg-white h-full border mx-auto">
+      <main className="w-[1440px] bg-white h-full border p-10 mx-auto">
+        <div className="flex justify-center mb-10">
+          <ProfilePic />
+        </div>
         <section className="">
           <div className="w-[80%] mx-auto">
             <LinkButton
--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -1,12 +1,17 @@
 import React from "react";
+import picture from "../../src/img/succy1.jpeg";
 
 const ProfilePic = (props) => {
   //   const { picture } = props;
   return (
-    <img
-      style={{ maxHeight: "225px", margin: "45px", borderRadius: "200px" }}
-      src={picture}
-    />
+    <div className="w-[88px] h-[88px] rounded-full">
+      <img
+        id="profile__img"
+        className="w-full h-full rounded-full cover"
+        src={picture}
+        alt="BigCo Inc. logo"
+      />
+    </div>
   );
 };
 
