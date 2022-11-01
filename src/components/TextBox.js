f9ecc549472c8eaa32811fa3c0f55b4d20ca5c15
success ibekwe <Successibekwe047@gmail.com>
2022-11-01 13:09:27 +0100

    added link to my twitter account

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -5,9 +5,16 @@ const TextBox = (props) => {
 
   return (
     <div className="my-6 text-center">
-      <h1 className="text-xl font-semibold text-gray-900" id="twitter">
+      <a
+        className="text-xl font-semibold text-gray-900"
+        id="twitter"
+        target="_blank"
+        rel="noreferrer"
+        href={"https://twitter.com/successibekwe1/"}
+      >
         {twitterUserName}
-      </h1>
+      </a>
+
       <p id="slack" className="hidden">
         {slackUserName}
       </p>
