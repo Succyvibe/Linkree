218f1a07aba54d0e9ddacf12284bc01db25b4842
success ibekwe <Successibekwe047@gmail.com>
2022-11-04 10:47:05 +0100

    Added contact button

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -59,13 +59,23 @@ const LinkButton = (props) => {
 
         <a
           id="book__design"
-          className="bg-[#f4f5f7] hover:bg-gray-400 transition duration-300 lg:w-[1152px] h-[76px] py-[24px] px-[32px] rounded-[8px] border border-[#EAECF0] text-gray-900 font-[600] text-center font-semibold"
+          className="bg-[#f4f5f7] hover:bg-gray-400 transition duration-300 lg:w-[1152px] h-[76px] py-[24px] px-[32px] rounded-[8px] border border-[#EAECF0] text-gray-900 font-[600] text-center"
           target="_blank"
           rel="noreferrer"
           href={"https://books.zuri.team/design-rules"}
           title="Best place to get amazingly free designs rules for your designs"
         >
           Design Books
+        </a>
+        <a
+          id="contact"
+          className="bg-[#f4f5f7] hover:bg-gray-400 transition duration-300 lg:w-[1152px] h-[76px] py-[24px] px-[32px] rounded-[8px] border border-[#EAECF0] text-gray-900 font-[600] text-center"
+          target="_blank"
+          rel="noreferrer"
+          href={"https://books.zuri.team/design-rules"}
+          title="Best place to get amazingly free designs rules for your designs"
+        >
+          Contact Me
         </a>
       </div>
     </div>
